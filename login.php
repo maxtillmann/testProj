e75ba96b8583d7073c12f9976e6544cea587fb73
Tsuzuku <gagandeepsandhu@protonmail.com>
2017-10-03 23:05:36 -0400

    Restructure project flow, and created AMQP Send Method

--- login.php
+++ login.php
@@ -1,54 +1,31 @@
 <?php
-	ini_set('display_errors', 'On');
-	error_reporting(E_ALL | E_STRICT);
-	
-	require 'vendor/autoload.php';
-	use GuzzleHttp\Psr7\Request;
-	use GuzzleHttp\Client;
-	
-	$ini_arr = parse_ini_file("amqpCreate.ini");
-	$url = $ini_arr['URL'];
-	//echo $url;
-	
-	//var_dump($_POST);
 
+ini_set('display_errors', 'On');
+error_reporting(E_ALL | E_STRICT);
+
+//require 'vendor/autoload.php';
+require_once __DIR__ . '/vendor/autoload.php';
+require 'amqpCreate.php.inc';
+use PhpAmqpLib\Connection\AMQPStreamConnection;
+use PhpAmqpLib\Message\AMQPMessage;
+	
 	if (!isset($_POST))
 	{
 		$msg = "Empty Form Submitted";
 		echo json_encode($msg);
 		exit(0);
 	}
-	
+
+	$qname = $_POST["type"];
+	//var_dump($_POST);
 	$credentials = json_encode($_POST);
-
-	//echo  $credentials;
-
-	/**************************************************************************
-	$ch = curl_init($url);
-	curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
-	curl_setopt($ch, CURLOPT_POSTFIELDS, $credentials);
-	curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-	curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, false);
-
-	//curl_close($ch);
+	$hostinfo = parse_ini_file("amqpCreate.ini");
 	
-	// echo curl_getinfo($ch) . '<br/>';
-	//echo curl_errno($ch) . '<br/>';
-	//echo curl_error($ch) . '<br/>';
-	//$output = curl_exec($ch);
-	***************************************************************************/
+
+	$amqp = new amqpCreate();
+	$amqp->amqpConstruct($hostinfo['HOST'],$hostinfo['PORT'],$hostinfo['USER'],$hostinfo['PASS']);
+	
+
+	$amqp->amqpSend($qname,$credentials);
 		
-	//$client = new Client(['debug'=>true,'exceptions'=>false,'verify' => false]);
-	$client = new Client(['verify' => false]);
-
-	//$output = $client->request('POST', 'http://httpbin.org/post', [ 'json' => $credentials]);
-	$output = $client->post($url, [ 'json' => $credentials]);
-	//$output = $client->post($url, [ 'json' => json_encode(['a' => 'b'])]);
-	//$output = $client->post('https://httpbin.org/post', [ 'json' => $credentials]);
-
-	
-	//echo "Output: " . $output->getBody()->getContents() . "<br />";
-	var_dump($output->getBody()->getContents());
-	var_dump($output->getBody()->getContents());
-	
 ?>
--- transfer.sh
+++
@@ -1,4 +0,0 @@
-
-sudo rm -r /var/www/html/testSite/ 
-
-sudo cp -r ../transportation /var/www/html/testSite
